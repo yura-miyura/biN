9a6fe0863193534e8341bb608d1004f04ea9fbca
Yurii Artym <140413112+yura-miyura@users.noreply.github.com>
2025-09-14 08:53:41 +0100

    upd

--- c03/ex01/ft_strncmp.c
+++ c03/ex01/ft_strncmp.c
@@ -28,15 +28,9 @@ int	ft_strncmp(char *s1, char *s2, unsigned int n)
 #include <string.h>
 int main(void)
 {
-<<<<<<< HEAD
-	unsigned int n = 2;
-	char *s1 = "heli";
-	char *s2 = "helu";
-=======
 	unsigned int n = 4;
 	char *s1 = "hell";
 	char *s2 = "hel";
->>>>>>> 0daa2a77083ee5ced4268ac646fde8bd9de8e38c
 
 	printf("%i\n", ft_strncmp(s1, s2, n));
 	printf("%i\n", strncmp(s1, s2, n));
