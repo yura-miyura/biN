ffb1960e7d786f2d1b5a95780f0f21ea52310546
Yurii Artym <140413112+yura-miyura@users.noreply.github.com>
2025-09-18 15:28:11 +0100

    upd

---
+++ c02/ex01/ft_strncpy.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yartym <yartym@student.42.fr>              #+#  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025-09-10 07:30:30 by yartym            #+#    #+#             */
+/*   Updated: 2025-09-10 07:30:30 by yartym           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*ft_strncpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int	index;
+
+	index = 0;
+	while (index < n && src[index])
+	{
+		dest[index] = src[index];
+		index++;
+	}
+	while (index < n)
+	{
+		dest[index] = '\0';
+		index++;
+	}
+	return (dest);
+}
+
+/* #include <stdio.h>
+#include <string.h>
+int main(void)
+{
+	unsigned int	n = 7;
+	char *src = "hello this function works";
+	char dest[n];
+	// printf("%s\n", ft_strncpy(dest, src, n));
+	printf("%s\n", strncpy(dest, src, n));
+}
+*/
