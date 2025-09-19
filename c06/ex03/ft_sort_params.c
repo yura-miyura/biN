857a5d39e7843ddc4986cac4a3c19bd56b49e0ad
Yurii Artym <140413112+yura-miyura@users.noreply.github.com>
2025-09-19 08:46:02 +0100

    c06/ex03

--- c06/ex03/ft_sort_params.c
+++ c06/ex03/ft_sort_params.c
@@ -12,24 +12,57 @@
 
 #include <unistd.h>
 
+int	ft_strcmp(char *s1, char *s2)
+{
+	while (*s1 && *s2 && *s1 == *s2)
+	{
+		s1++;
+		s2++;
+	}
+	return (*s1 - *s2);
+}
+
+char	**ft_sort(int argc, char **argv)
+{
+	int		i;
+	int		j;
+	int		cmp;
+	char	*tmp;
+
+	i = 1;
+	while (i < argc)
+	{
+		j = i + 1;
+		while (j < argc)
+		{
+			cmp = ft_strcmp(argv[i], argv[j]);
+			if (cmp > 0)
+			{
+				tmp = argv[i];
+				argv[i] = argv[j];
+				argv[j] = tmp;
+			}
+			j++;
+		}
+		i++;
+	}
+	return (argv);
+}
+
 int	main(int argc, char **argv)
 {
 	int	i;
-	int j;
-	int	len;
+	int	j;
+	char **sorted_argv;
 
-	len = argc - 1;
-	while (len > 0)
+	sorted_argv = ft_sort(argc, argv);
+	i = 1;
+	while (i < argc)
 	{
-		i = 0;
-		while (argv[len][i])
-		{
-			j = 0;
-			while (argv[len][j])
-
-				write(1, argv[len] + i++, 1);
-		}
+		j = 0;
+		while (sorted_argv[i][j])
+			write(1, sorted_argv[i] + j++, 1);
 		write(1, "\n", 1);
-		len--;
+		i++;
 	}
 }
